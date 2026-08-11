cdb40f27397bab2b8a2da9abe8038178ba4eef06
Francisco Hernández <fran.hrdz93@gmail.com>
2026-08-11 17:46:52 -0600

    Mantiene Netlify y añade isotipo PWA

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "presenta-v9";
-const APP_SHELL = ["/", "/?remote=1", "/manifest.webmanifest", "/favicon.svg", "/icon-192.png", "/icon-512.png"];
+const CACHE_NAME = "presenta-v10";
+const APP_SHELL = ["/", "/?remote=1", "/manifest.webmanifest", "/favicon.svg", "/icon-192.png", "/icon-512.png", "/icon-maskable-192.png", "/icon-maskable-512.png", "/apple-touch-icon.png"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)));
